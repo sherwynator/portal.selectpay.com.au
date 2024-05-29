958d02be6b78952757e13af0c9e39247f2291f6b
Sherwyn Ayao <sherwyn.ayao@gmail.com>
2024-05-29 20:39:18 +0800

    v1.1.15

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,10 +3,10 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"version.json": "4184b18831bcfeb7c19b5d0cf04e660e",
-"index.html": "5f05188b977ad8fd35d142d6cce2f030",
-"/": "5f05188b977ad8fd35d142d6cce2f030",
-"main.dart.js": "d8142db3a51796bf4541bb13cd35f532",
+const RESOURCES = {"version.json": "4a93cdac500df7cb8ef4b60ff55f2482",
+"index.html": "25f815292b20c5e83a02510d6477bff6",
+"/": "25f815292b20c5e83a02510d6477bff6",
+"main.dart.js": "f0bc092e0351422cccc3f5da92b305c9",
 "flutter.js": "c71a09214cb6f5f8996a531350400a9a",
 "favicon.png": "2cfa88ac2ceb26ec5c0586bf29a04550",
 "icons/Icon-192.png": "16afb854744b2468e82d155c875b37c5",
